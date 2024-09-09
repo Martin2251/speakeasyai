1ff282e5e8c87af976fd502bcbfd8fc1c10199a2
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:22:42 +0100

    H2

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Banner = () => {
   return (
    <section className='lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in'>
-    Hi Banner
+    <h1>Turn your words into <span className='underline underline-offset-8 decoration-dashed decoration-purple-200'>Captivating</span> blog posts</h1>
+    <h2 className=''>Convert your video or voice into a Blog Post in seconds with the power of AI</h2>
+
    </section>
   )
 }
