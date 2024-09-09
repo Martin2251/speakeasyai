d09e816dd287ed1005fe7622d8d386f0cd7744d0
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:19:26 +0100

    section

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>
-      
-    </div>
+   <section className='lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in'>
+    Hi Banner
+   </section>
   )
 }
 
