df4ab1ceaf6cabf8eb49f378ddac80a11da76dbe
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:13:42 +0100

    class on main

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <main className="py-12">
+  <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px)] [background-size:16px_16px]">
     <Button variant={"destructive"} >hi button</Button>
   </main>
   );
