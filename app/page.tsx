a486aa15294d8f721c1dc10a3231b179f272f3f8
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:15:37 +0100

    new page components

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
   <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px)] [background-size:16px_16px]">
-    <Button variant={"destructive"} >hi button</Button>
+    <Banner  />
+    <Divider  />
+    <HowItWorks  />
+    <Divider  />
+    <Pricing  />
+    <Divider  />
+    <Footer  />
   </main>
   );
 }
