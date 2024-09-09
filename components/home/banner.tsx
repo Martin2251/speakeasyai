46fa8016c8e6345a15ad79ece3ef6efe7ecae079
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:46:05 +0100

    banner

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const Banner = () => {
   return (
    <section className='lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in'>
     <h1 className='py-6 text-center'>Turn your words into <span className='underline underline-offset-8 decoration-dashed decoration-purple-200'>Captivating</span> blog posts</h1>
     <h2 className='text-center px-4 lg:px-0 lg:max-w-4xl'>Convert your video or voice into a Blog Post in seconds with the power of AI</h2>
+    <Button variant={'link'}>
+        <Link href="/pricing">Get Speak Easy</Link>
+    </Button>
 
    </section>
   )
