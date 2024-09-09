a9addf8de4d34954b1da43148f59d999152d693d
Martin Lowe <martinlowe673@gmail.com>
2024-09-09 07:49:55 +0100

    button styles

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
    <section className='lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in'>
     <h1 className='py-6 text-center'>Turn your words into <span className='underline underline-offset-8 decoration-dashed decoration-purple-200'>Captivating</span> blog posts</h1>
     <h2 className='text-center px-4 lg:px-0 lg:max-w-4xl'>Convert your video or voice into a Blog Post in seconds with the power of AI</h2>
-    <Button variant={'link'}>
+    <Button variant={'link'} className='mt-6 text-xl rounded-full px-12 py-8 lg:mt-20 bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-indigo-600'>
         <Link href="/pricing">Get Speak Easy</Link>
     </Button>
 
