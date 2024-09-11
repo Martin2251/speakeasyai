bb532e3ed6328ffc7f8cb3c523f75d5fdf326f0d
Martin Lowe <martinlowe673@gmail.com>
2024-09-11 08:01:28 +0100

    animation

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
   <nav className='container flex items-center justify-between px-8 py-4 mx-auto'>
-    <Link href="/">SpeakEasy
+    <Link href="/" className='transition-colors duration-200 text-gray-600 hover:text-purple-500'><span className='flex items-center gap-2 shrink-0'><Ghost className="hover:rotate-12 transform transition duration-200 ease-in-out"  /></span>SpeakEasy
     </Link>
 
   </nav>
