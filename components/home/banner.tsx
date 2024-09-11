6e140d6cd30131f4aea9049305333e6c67ba5d1a
Martin Lowe <martinlowe673@gmail.com>
2024-09-11 07:33:36 +0100

    banner

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import Link from 'next/link'
+import { ArrowLeftRightIcon, ArrowRightIcon } from 'lucide-react'
 
 const Banner = () => {
   return (
@@ -8,7 +9,7 @@ const Banner = () => {
     <h1 className='py-6 text-center'>Turn your words into <span className='underline underline-offset-8 decoration-dashed decoration-purple-200'>Captivating</span> blog posts</h1>
     <h2 className='text-center px-4 lg:px-0 lg:max-w-4xl'>Convert your video or voice into a Blog Post in seconds with the power of AI</h2>
     <Button variant={'link'} className='mt-6 text-xl rounded-full px-12 py-8 lg:mt-20 bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-indigo-600 hover:to-purple-600 text-white font-bold shadow-lg hover:no-underline'>
-        <Link href="/pricing"><span className='relative'>Get Speak Easy</span></Link>
+        <Link href="/pricing"><span className='relative'>Get Speak Easy <ArrowRightIcon className='absolute -bottom-1 left-0 w-full h-0.5 bg-white transform scale-x-0 transition-transform duration-300 origin-left group-hover:scale-x-100' /> </span></Link>
     </Button>
 
    </section>
