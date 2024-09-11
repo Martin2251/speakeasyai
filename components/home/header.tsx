406f7e3fb7a74a929c1726e31f7408c86712e5c3
Martin Lowe <martinlowe673@gmail.com>
2024-09-11 08:04:40 +0100

    spans

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import { Ghost } from 'lucide-react'
 
 const Header = () => {
   return (
   <nav className='container flex items-center justify-between px-8 py-4 mx-auto'>
-    <Link href="/" className='transition-colors duration-200 text-gray-600 hover:text-purple-500'><span className='flex items-center gap-2 shrink-0'><Ghost className="hover:rotate-12 transform transition duration-200 ease-in-out"  /></span>SpeakEasy
+    <Link href="/" className='transition-colors duration-200 text-gray-600 hover:text-purple-500'>
+    <span className='flex items-center gap-2 shrink-0'>
+        <Ghost className="hover:rotate-12 transform transition duration-200 ease-in-out"  />
+        <span className='font-extrabold text-lg'>SpeakEasy</span>
+        </span>
     </Link>
 
   </nav>
